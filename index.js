229100fc0d5203def857b080738a4a63e8191487
Parham Oghabi <parham.oghabi@mail.utoronto.ca>
2016-07-16 00:06:49 -0400

    Added the database files again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-//var pg = require('pg');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -24,17 +24,17 @@ app.listen(app.get('port'), function() {
 
  
 
-//app.get('/db', function (request, response) {
-//        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//                   client.query(' SELECT * FROM Users;', function(err, result)
-//
-//                                {
-//                                done();
-//                                if (err)
-//                                { console.error(err); response.send("Error " + err); }
-//                                else
-//                                { response.render('pages/db', {results: result.rows} ); }
-//                                });
-//                   });
-//        });
-//
+app.get('/db', function (request, response) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                   client.query(' SELECT * FROM Users;', function(err, result)
+
+                                {
+                                done();
+                                if (err)
+                                { console.error(err); response.send("Error " + err); }
+                                else
+                                { response.render('pages/db', {results: result.rows} ); }
+                                });
+                   });
+        });
+
