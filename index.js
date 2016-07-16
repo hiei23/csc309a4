7f937493dcf3b57516e0a801978e60e314ea0904
Parham Oghabi <parham.oghabi@mail.utoronto.ca>
2016-07-16 00:13:37 -0400

    Trying to connect to old db

--- index.js
+++ index.js
@@ -24,17 +24,17 @@ app.listen(app.get('port'), function() {
 
  
 
-//app.get('/db', function (request, response) {
-//        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//                   client.query(' SELECT * FROM Users;', function(err, result)
-//
-//                                {
-//                                done();
-//                                if (err)
-//                                { console.error(err); response.send("Error " + err); }
-//                                else
-//                                { response.render('pages/db', {results: result.rows} ); }
-//                                });
-//                   });
-//        });
-//
+app.get('/db', function (request, response) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                   client.query(' SELECT * FROM Users;', function(err, result)
+
+                                {
+                                done();
+                                if (err)
+                                { console.error(err); response.send("Error " + err); }
+                                else
+                                { response.render('pages/db', {results: result.rows} ); }
+                                });
+                   });
+        });
+
