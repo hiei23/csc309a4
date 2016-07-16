f754ca26dbd3933402e384929e6579d952225bdd
Parham Oghabi <parham.oghabi@mail.utoronto.ca>
2016-07-15 21:32:57 -0400

    Fixed an Image

--- app.js
+++ app.js
@@ -1,57 +1,82 @@
 var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var pg = require('pg');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('port', (process.env.PORT || 5000));
+
+
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'front_end')));
+
+app.use(express.static(__dirname + '/front_end'));
+
+app.get('/', function (req, res)
+                                {
+                                     res.sendfile(__dirname + '/front_end/index.html');
+                                }
+        );
 
 
 
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+app.get('/db', function (request, response) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                   client.query('Set search_path to tsports;  SELECT * FROM Users;', function(err, result)
+                                
+                                {
+                                done();
+                                if (err)
+                                { console.error(err); response.send("Error " + err); }
+                                else
+                                { response.render('pages/db', {results: result.rows} ); }
+                                });
+                   });
+        });
 
 
-module.exports = app;
+app.listen(3000, function ()
+                           {
+                             console.log('Example app listening on port 3000!');
+                           }
+           );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//connection.end();
+//lit-island-12585.herokuapp.com
+////var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+////
+////var client = new pg.Client(connectionString);
+////client.connect();
+////var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+////query.on('end', function() { client.end(); });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
