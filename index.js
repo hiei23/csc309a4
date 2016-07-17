9a3eb14dcf5231b6f98a5fa1d1411341e64c1e40
JING WANG <jimwong234@gmail.com>
2016-07-16 22:58:53 -0400

    delete register/login main_nav

--- index.js
+++ index.js
@@ -11,30 +11,26 @@ app.use(express.static(__dirname + '/front_end'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res)
-        {
-        res.sendfile(__dirname + '/front_end/index.html');
-        }
-        );
-
+app.get('/', function (req, res){
+    res.sendfile(__dirname + '/front_end/index.html');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
- 
-
 app.get('/db', function (request, response) {
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-                   client.query(' SELECT * FROM Users;', function(err, result)
-
-                                {
-                                done();
-                                if (err)
-                                { console.error(err); response.send("Error " + err); }
-                                else
-                                { response.render('pages/db', {results: result.rows} ); }
-                                });
-                   });
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query(' SELECT * FROM Users;', function(err, result){
+            done();
+            if(err){ 
+                console.error(err); 
+                response.send("Error " + err); 
+            }
+            else{ 
+                response.render('pages/db', {results: result.rows} ); 
+            }
         });
+    });
+});
 
